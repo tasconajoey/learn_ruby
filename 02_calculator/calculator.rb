292e79c59b43ef9b8b4261cd8a5139a50f5e08fd
tasconajoey <joey.tascona@gmail.com>
2017-01-23 13:39:11 -0500

    Finish third exercise

---
+++ 02_calculator/calculator.rb
@@ -0,0 +1,19 @@
+def add(x, y)
+  x + y
+end
+def subtract(x, y)
+  x - y
+end
+def sum(array)
+  sum = 0
+  array.each do |num|
+    sum += num
+  end
+  sum
+end
+def product(x, y)
+  x * y
+end
+def product1(x, y, m, c, s, d, t)
+  x * y * m * c * s * d * t
+end
--- 02_calculator/test_calculator.rb
+++ 02_calculator/test_calculator.rb
@@ -45,9 +45,11 @@ class TestCalculator < MiniTest::Test
   # If you feel you're up for the challenge, fill in the missing code in the tests below,
   # based off the descriptions, and get them to pass.
   def test_multiplies_two_numbers
+    assert_equal 20, product(10, 2)
   end
 
   def test_multiplies_several_numbers
+    assert_equal 100, product1(1, 1, 10, 10, 1, 1, 1)
   end
 
 # http://en.wikipedia.org/wiki/Factorial
